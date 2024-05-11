12c3981285fcd2c5a6237d0fc9dfbe6554413218
piecucci <piecucci@gmail.com>
2024-05-11 15:26:40 +0200

    Update RandomCards.java

--- RandomCards.java
+++ RandomCards.java
@@ -1,77 +1,29 @@
 public class RandomCards {
     public static void main(String[] args) {
         for (int i = 0; i < 5; ++i) {
-            /*
-             * Generate random numbers between 1 and 4 (inclusive) to represent the 4 suits.
-             The Math.random() method returns a random double value between 0.0 (inclusive) and 1.0 (exclusive).
-             By multiplying this value by 4 and casting it to an integer, we get a random integer between 0 and 3.
-             Adding 1 shifts the range to be between 1 and 4.
-             */
-            int col = (int) (Math.random() * 4) + 1;
-            int rnk = (int) (Math.random() * 13) + 2;
+            // Generate random integers for suit and rank
+            int col = (int) (Math.random() * 4) + 1; // 1 to 4 (clubs, diamonds, hearts, spades)
+            int rnk = (int) (Math.random() * 13) + 2; // 2 to 14 (deuce to Ace)
 
-            String color;
-            switch (col) {
-                case 1:
-                    color = "Clubs";
-                    break;
-                case 2:
-                    color = "Diamonds";
-                    break;
-                case 3:
-                    color = "Hearts";
-                    break;
-                case 4:
-                    color = "Spades";
-                    break;
-                default:
-                    throw new IllegalStateException("Unexpected value: " + col);
-            }
+            // Define strings for suit and rank
+            String color = switch (col) {
+                case 1 -> "Clubs";
+                case 2 -> "Diamonds";
+                case 3 -> "Hearts";
+                case 4 -> "Spades";
+                default -> "Unknown";
+            };
 
-            String rank;
-            switch (rnk) {
-                case 2:
-                    rank = "Two";
-                    break;
-                case 3:
-                    rank = "Three";
-                    break;
-                case 4:
-                    rank = "Four";
-                    break;
-                case 5:
-                    rank = "Five";
-                    break;
-                case 6:
-                    rank = "Six";
-                    break;
-                case 7:
-                    rank = "Seven";
-                    break;
-                case 8:
-                    rank = "Eight";
-                    break;
-                case 9:
-                    rank = "Nine";
-                    break;
-                case 10:
-                    rank = "Ten";
-                    break;
-                case 11:
-                    rank = "Jack";
-                    break;
-                case 12:
-                    rank = "Queen";
-                    break;
-                case 13:
-                    rank = "King";
-                    break;
-                case 14:
-                    rank = "Ace";
-                    break;
-                default:
-                    throw new IllegalStateException("Unexpected value: " + rnk);
-            }
+            String rank = switch (rnk) {
+                case 2 -> "Deuce";
+                case 3 -> "Trey";
+                case 4, 5, 6, 7, 8, 9, 10 -> String.valueOf(rnk);
+                case 11 -> "Jack";
+                case 12 -> "Queen";
+                case 13 -> "King";
+                case 14 -> "Ace";
+                default -> "Unknown";
+            };
 
             System.out.println(rank + " of " + color);
         }
